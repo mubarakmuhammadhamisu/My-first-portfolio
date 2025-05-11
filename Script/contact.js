d741ae6fc78c3b21e0f0d19a298e7ee797c4807f
mubarak <hamisumubaral447@gmail.com>
2025-05-11 22:43:46 +0100

    adding time on the forn clear

--- Script/contact.js
+++ Script/contact.js
@@ -2,7 +2,7 @@ document.querySelector("form").addEventListener("submit", function (e) {
   // Wait a moment for the submission to complete
   setTimeout(() => {
     document.querySelectorAll(".input").forEach(input => input.value = "");
-  }, 500); // Adjust the delay if needed
+  }, 1000); // Adjust the delay if needed
 });
 
 
