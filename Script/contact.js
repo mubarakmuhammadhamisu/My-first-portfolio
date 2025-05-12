cc0c6b0b605213192947233934087a0a535ba57d
mubarak <hamisumubaral447@gmail.com>
2025-05-12 17:19:15 +0100

    sepret css

--- Script/contact.js
+++ Script/contact.js
@@ -4,7 +4,3 @@ document.querySelector("form").addEventListener("submit", function (e) {
     document.querySelectorAll(".input").forEach(input => input.value = "");
   }, 1000); // Adjust the delay if needed
 });
-
-
-//<input type="hidden" name="redirect" value="https://yourdomain.com/thank-you.html">
-
